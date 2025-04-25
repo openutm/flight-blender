fb071340a22ab96adf5da10f2711544113dac166
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2025-04-25 14:29:02 +0100

    Update tasks.py

--- flight_declaration_operations/tasks.py
+++ flight_declaration_operations/tasks.py
@@ -38,12 +38,11 @@ load_dotenv(find_dotenv())
 def submit_flight_declaration_to_dss_async(flight_declaration_id: str):
     my_dss_opint_creator = DSSOperationalIntentsCreator(flight_declaration_id)
     my_database_reader = FlightBlenderDatabaseReader()
-    r = get_redis()
     my_database_writer = FlightBlenderDatabaseWriter()
 
     start_end_time_validated = my_dss_opint_creator.validate_flight_declaration_start_end_time()
 
-    logger.info("Flight Operation Validation status %s" % start_end_time_validated)
+    logger.info("Flight Operation start end time status %s" % start_end_time_validated)
 
     if not start_end_time_validated:
         logger.error(
@@ -59,143 +58,144 @@ def submit_flight_declaration_to_dss_async(flight_declaration_id: str):
             message_text=validation_not_ok_msg,
             level="error",
         )
-    else:
-        validation_ok_msg = "Flight Operation with ID {operation_id} validated for start and end time, submitting to DSS..".format(
+        return
+
+    validation_ok_msg = "Flight Operation with ID {operation_id} validated for start and end time, submitting to DSS..".format(
+        operation_id=flight_declaration_id
+    )
+    send_operational_update_message.delay(
+        flight_declaration_id=flight_declaration_id,
+        message_text=validation_ok_msg,
+        level="info",
+    )
+    logger.info("Submitting to DSS..")
+
+    opint_submission_result = my_dss_opint_creator.submit_flight_declaration_to_dss()
+
+    if opint_submission_result.status_code == 500:
+        logger.error("Error in submitting Flight Declaration to the DSS %s" % opint_submission_result.status)
+
+        dss_submission_error_msg = (
+            "Flight Operation with ID {operation_id} could not be submitted to the DSS, check the Auth server and / or the DSS URL".format(
+                operation_id=flight_declaration_id
+            )
+        )
+        send_operational_update_message.delay(
+            flight_declaration_id=flight_declaration_id,
+            message_text=dss_submission_error_msg,
+            level="error",
+        )
+
+    elif opint_submission_result.status_code in [400, 409, 401, 412]:
+        logger.error("Error in submitting Flight Declaration to the DSS %s" % opint_submission_result.status)
+
+        dss_submission_error_msg = (
+            "Flight Operation with ID {operation_id} was rejected by the DSS, there was a error in data submitted by Flight Blender".format(
+                operation_id=flight_declaration_id
+            )
+        )
+        send_operational_update_message.delay(
+            flight_declaration_id=flight_declaration_id,
+            message_text=dss_submission_error_msg,
+            level="error",
+        )
+
+    elif opint_submission_result.status_code == 201:
+        logger.info("Successfully submitted Flight Declaration to the DSS %s" % opint_submission_result.status)
+
+        submission_success_msg = "Flight Operation with ID {operation_id} submitted successfully to the DSS".format(
             operation_id=flight_declaration_id
         )
         send_operational_update_message.delay(
             flight_declaration_id=flight_declaration_id,
-            message_text=validation_ok_msg,
+            message_text=submission_success_msg,
             level="info",
         )
-        logger.info("Submitting to DSS..")
 
-        opint_submission_result = my_dss_opint_creator.submit_flight_declaration_to_dss()
+        ###### Change via new state check helper
 
-        if opint_submission_result.status_code == 500:
-            logger.error("Error in submitting Flight Declaration to the DSS %s" % opint_submission_result.status)
+        flight_declaration = my_database_reader.get_flight_declaration_by_id(flight_declaration_id=flight_declaration_id)
 
-            dss_submission_error_msg = (
-                "Flight Operation with ID {operation_id} could not be submitted to the DSS, check the Auth server and / or the DSS URL".format(
-                    operation_id=flight_declaration_id
-                )
-            )
-            send_operational_update_message.delay(
-                flight_declaration_id=flight_declaration_id,
-                message_text=dss_submission_error_msg,
-                level="error",
-            )
+        fa = my_database_reader.get_flight_operational_intent_reference_by_flight_declaration_obj(flight_declaration=flight_declaration)
 
-        elif opint_submission_result.status_code in [400, 409, 401, 412]:
-            logger.error("Error in submitting Flight Declaration to the DSS %s" % opint_submission_result.status)
+        logger.info("Saving created operational intent details..")
+        created_opint = fa.id
+        view_r_bounds = flight_declaration.bounds
+        operational_intent_full_details = OperationalIntentStorage(
+            bounds=view_r_bounds,
+            start_datetime=flight_declaration.start_datetime.isoformat(),
+            end_datetime=flight_declaration.end_datetime.isoformat(),
+            alt_max=50,
+            alt_min=25,
+            success_response=opint_submission_result.dss_response,
+            operational_intent_details=json.loads(flight_declaration.operational_intent),
+        )
 
-            dss_submission_error_msg = (
-                "Flight Operation with ID {operation_id} was rejected by the DSS, there was a error in data submitted by Flight Blender".format(
-                    operation_id=flight_declaration_id
-                )
-            )
-            send_operational_update_message.delay(
-                flight_declaration_id=flight_declaration_id,
-                message_text=dss_submission_error_msg,
-                level="error",
-            )
+        my_database_writer.update_flight_operational_intent_reference_with_dss_response(
+            flight_declaration=flight_declaration,
+            dss_operational_intent_reference_id=str(created_opint),
+            dss_response=opint_submission_result.dss_response,
+            ovn=opint_submission_result.dss_response.operational_intent_reference.ovn,
+        )
 
-        elif opint_submission_result.status_code == 201:
-            logger.info("Successfully submitted Flight Declaration to the DSS %s" % opint_submission_result.status)
-
-            submission_success_msg = "Flight Operation with ID {operation_id} submitted successfully to the DSS".format(
-                operation_id=flight_declaration_id
-            )
-            send_operational_update_message.delay(
-                flight_declaration_id=flight_declaration_id,
-                message_text=submission_success_msg,
-                level="info",
-            )
-
-            ###### Change via new state check helper
-
-            flight_declaration = my_database_reader.get_flight_declaration_by_id(flight_declaration_id=flight_declaration_id)
-
-            fa = my_database_reader.get_flight_operational_intent_reference_by_flight_declaration_obj(flight_declaration=flight_declaration)
-
-            logger.info("Saving created operational intent details..")
-            created_opint = fa.id
-            view_r_bounds = flight_declaration.bounds
-            operational_intent_full_details = OperationalIntentStorage(
-                bounds=view_r_bounds,
-                start_datetime=flight_declaration.start_datetime.isoformat(),
-                end_datetime=flight_declaration.end_datetime.isoformat(),
-                alt_max=50,
-                alt_min=25,
-                success_response=opint_submission_result.dss_response,
-                operational_intent_details=json.loads(flight_declaration.operational_intent),
-            )
-
-            my_database_writer.update_flight_operational_intent_reference_with_dss_response(
-                flight_declaration=flight_declaration,
-                dss_operational_intent_reference_id=str(created_opint),
-                dss_response=opint_submission_result.dss_response,
-                ovn=opint_submission_result.dss_response.operational_intent_reference.ovn,
-            )
-
-            logger.info("Changing operation state..")
-            original_state = flight_declaration.state
-            accepted_state = OPERATION_STATES[1][0]
-            my_conformance_helper = FlightOperationConformanceHelper(flight_declaration_id=flight_declaration_id)
-            transition_valid = my_conformance_helper.verify_operation_state_transition(
-                original_state=original_state,
+        logger.info("Changing operation state..")
+        original_state = flight_declaration.state
+        accepted_state = OPERATION_STATES[1][0]
+        my_conformance_helper = FlightOperationConformanceHelper(flight_declaration_id=flight_declaration_id)
+        transition_valid = my_conformance_helper.verify_operation_state_transition(
+            original_state=original_state,
+            new_state=accepted_state,
+            event="dss_accepts",
+        )
+        if transition_valid:
+            my_database_writer.update_flight_operation_state(flight_declaration_id=flight_declaration_id, state=accepted_state)
+            logger.info("The state change transition to Accepted state from current state Created is valid..")
+            flight_declaration.add_state_history_entry(
                 new_state=accepted_state,
-                event="dss_accepts",
-            )
-            if transition_valid:
-                my_database_writer.update_flight_operation_state(flight_declaration_id=flight_declaration_id, state=accepted_state)
-                logger.info("The state change transition to Accepted state from current state Created is valid..")
-                flight_declaration.add_state_history_entry(
-                    new_state=accepted_state,
-                    original_state=original_state,
-                    notes="Successfully submitted to the DSS",
-                )
-
-            submission_state_updated_msg = "Flight Operation with ID {operation_id} has a updated state: Accepted. ".format(
-                operation_id=flight_declaration_id
-            )
-            send_operational_update_message.delay(
-                flight_declaration_id=flight_declaration_id,
-                message_text=submission_state_updated_msg,
-                level="info",
+                original_state=original_state,
+                notes="Successfully submitted to the DSS",
             )
 
-            logger.info("Notifying subscribers..")
+        submission_state_updated_msg = "Flight Operation with ID {operation_id} has a updated state: Accepted. ".format(
+            operation_id=flight_declaration_id
+        )
+        send_operational_update_message.delay(
+            flight_declaration_id=flight_declaration_id,
+            message_text=submission_state_updated_msg,
+            level="info",
+        )
 
-            subscribers = opint_submission_result.dss_response.subscribers
-            if subscribers:
-                for subscriber in subscribers:
-                    subscriptions_raw = subscriber["subscriptions"]
-                    uss_base_url = subscriber["uss_base_url"]
-                    flight_blender_base_url = env.get("FLIGHTBLENDER_FQDN", "http://localhost:8000")
+        logger.info("Notifying subscribers..")
 
-                    if uss_base_url != flight_blender_base_url:  # There are others who are subscribesd, not just ourselves
-                        subscriptions = from_dict(data_class=SubscriptionState, data=subscriptions_raw)
-                        op_int_details = from_dict(
-                            data_class=OperationalIntentUSSDetails,
-                            data=json.loads(flight_declaration.operational_intent),
-                        )
-                        operational_intent = OperationalIntentDetailsUSSResponse(
-                            reference=opint_submission_result.dss_response.operational_intent_reference,
-                            details=op_int_details,
-                        )
-                        post_notification_payload = NotifyPeerUSSPostPayload(
-                            operational_intent_id=created_opint,
-                            operational_intent=operational_intent,
-                            subscriptions=subscriptions,
-                        )
-                        # Notify Subscribers
-                        my_dss_opint_creator.notify_peer_uss(
-                            uss_base_url=uss_base_url,
-                            notification_payload=post_notification_payload,
-                        )
+        subscribers = opint_submission_result.dss_response.subscribers
+        if subscribers:
+            for subscriber in subscribers:
+                subscriptions_raw = subscriber["subscriptions"]
+                uss_base_url = subscriber["uss_base_url"]
+                flight_blender_base_url = env.get("FLIGHTBLENDER_FQDN", "http://localhost:8000")
 
-        logger.info("Details of the submission status %s" % opint_submission_result.message)
+                if uss_base_url != flight_blender_base_url:  # There are others who are subscribesd, not just ourselves
+                    subscriptions = from_dict(data_class=SubscriptionState, data=subscriptions_raw)
+                    op_int_details = from_dict(
+                        data_class=OperationalIntentUSSDetails,
+                        data=json.loads(flight_declaration.operational_intent),
+                    )
+                    operational_intent = OperationalIntentDetailsUSSResponse(
+                        reference=opint_submission_result.dss_response.operational_intent_reference,
+                        details=op_int_details,
+                    )
+                    post_notification_payload = NotifyPeerUSSPostPayload(
+                        operational_intent_id=created_opint,
+                        operational_intent=operational_intent,
+                        subscriptions=subscriptions,
+                    )
+                    # Notify Subscribers
+                    my_dss_opint_creator.notify_peer_uss(
+                        uss_base_url=uss_base_url,
+                        notification_payload=post_notification_payload,
+                    )
+
+    logger.info("Details of the submission status %s" % opint_submission_result.message)
 
 
 @app.task(name="send_operational_update_message")
