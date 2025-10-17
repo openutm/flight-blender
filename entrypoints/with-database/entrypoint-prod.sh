bc0092b788a7f83a15ac70157a3a96240caa31a9
Mika Tuupola <tuupola@appelsiini.net>
2025-10-17 13:25:18 +0300

    Remove hardcoded Posgres port

--- entrypoints/with-database/entrypoint-prod.sh
+++ entrypoints/with-database/entrypoint-prod.sh
@@ -3,7 +3,7 @@
 source .venv/bin/activate
 
 echo Waiting for DBs...
-if ! wait-for-it --parallel --service $REDIS_HOST:$REDIS_PORT --service $POSTGRES_HOST:5432; then
+if ! wait-for-it --parallel --service $REDIS_HOST:$REDIS_PORT --service $POSTGRES_HOST:$POSTGRES_PORT; then
     exit
 fi
 
