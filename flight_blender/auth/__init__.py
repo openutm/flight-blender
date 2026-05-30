694f9d7117b8f3e2f629b55fb96a4699b474537f
Attila Kobor <atti92@gmail.com>
2026-05-30 21:29:24 +0200

    update

--- flight_blender/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from flight_blender.auth.dss_auth_helper import AuthorityCredentialsGetter
-from flight_blender.auth.jwt_bearer import ReadDep, WriteDep, require_scope
-
-__all__ = [
-    "AuthorityCredentialsGetter",
-    "ReadDep",
-    "WriteDep",
-    "require_scope",
-]
