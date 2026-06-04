97eef2e44012bd0cf35f91854617883f5176d413
Attila Kobor <atti92@gmail.com>
2026-06-04 22:39:25 +0200

    add django setup back

--- src/flight_blender/asgi.py
+++ src/flight_blender/asgi.py
@@ -1,3 +1,10 @@
-from flight_blender.api.main import create_fastapi_app
+import os
+
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flight_blender.settings")
+django.setup()
+
+from flight_blender.api.main import create_fastapi_app  # noqa: E402
 
 application = create_fastapi_app()
