c34be0289d86bed0d0afe9a17ef36f6f76f9188c
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2026-07-06 15:51:21 +0100

    Added the home view to show the default endpoint

--- src/flight_blender/api/routers/misc_api.py
+++ src/flight_blender/api/routers/misc_api.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
 
 from flight_blender.services import misc_svc
 
 router = APIRouter()
+
+templates = Jinja2Templates(directory="src/flight_blender/templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(request=request, name="homebase/home.html", context={"request": request})
 
 
 @router.get("/ping")
